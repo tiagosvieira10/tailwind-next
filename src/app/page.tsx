1ad80610b94e1833ff99904e9918592d6b5be138
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-03-21 16:52:48 -0300

    inicio fineshed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <div className="flex items-center text-center justify-center bg-sky-950 h-screen">
       <div className="items-center m-auto p-44 font-sans">
-        <h1 className="text-6xl font-black">
+        <h1 className="text-6xl text-slate-50 font-black">
           Rapidly build modern websites without ever leaving your HTML.
         </h1>
-        <p className="text-xl p-4 px-48 m-auto">
-          A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.
+        <p className="text-xl p-4 px-24 mb-10 text-slate-400 m-auto">
+          A utility-first CSS framework packed with classes like <strong className="text-sky-500">flex</strong>, <strong className="text-sky-500">pt-4</strong>, <strong className="text-sky-500">text-center</strong> and <strong className="text-sky-500">rotate-90</strong> that can be composed to build any design, directly in your markup.
         </p>
         <button className="bg-sky-500 text-white px-8 py-4 rounded-md font-medium">
           Get started
