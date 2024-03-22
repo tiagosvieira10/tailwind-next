e2a76d154444fe6d0abde8980dff24434c685595
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-03-22 15:32:59 -0300

    prettier e eslint config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="flex items-center text-center justify-center bg-sky-950 h-screen">
-      <div className="items-center m-auto p-44 font-sans">
-        <h1 className="text-6xl text-slate-50 font-black">
-          Rapidly build modern websites without ever leaving your HTML.
-        </h1>
-        <p className="text-xl p-4 px-24 mb-10 text-slate-400 m-auto">
-          A utility-first CSS framework packed with classes like <strong className="text-sky-500">flex</strong>, <strong className="text-sky-500">pt-4</strong>, <strong className="text-sky-500">text-center</strong> and <strong className="text-sky-500">rotate-90</strong> that can be composed to build any design, directly in your markup.
-        </p>
-        <button className="bg-sky-500 hover:bg-sky-400 text-white px-8 py-4 rounded-md font-medium">
-          Get started
-        </button>
-      </div>
-    </div>
-  )
+  return <h1 className="flex">Hello World</h1>
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,16 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/**/*.tsx",
-  ],
+/** @type {import('tailwindcss').config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      gridTemplateColumns: {
+        app: 'minmax(18rem 20rem) 1fr',
+      },
+
+      maxWidth: {
+        app: '700px',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
